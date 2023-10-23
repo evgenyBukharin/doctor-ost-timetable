dd961f61eccd8f13a55c49b41d5f9606f96a7582
Evgeniy Bukharin <buharinzxc123@gmail.com>
2023-10-24 00:21:58 +0500

    hz

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,108 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="timetable__container">
+        <div class="timetable__content">
+            <Header></Header>
+            <div class="timetable__flex">
+                <TimetableColumn />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import TimetableColumn from './components/TimetableColumn.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    components: { Header, TimetableColumn },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+    box-sizing: border-box;
+}
+*,
+*::before,
+*::after {
+    box-sizing: inherit;
+}
+:root {
+    --font-family: 'Montserrat', sans-serif;
+    --white-color: #fff;
+    --grey-color: #eee;
+    --dark-grey-color: #ececec;
+    --black-color: #000;
+    --blue-color: #299b9c;
+    --orange-color: #e15335;
+}
+body {
+    background: var(--grey-color);
+    min-height: 100vh;
+    margin: 0;
+    padding: 0;
+    font-family: var(--font-family, sans-serif);
+}
+.timetable {
+    &__container {
+        max-width: 1440px;
+        margin: 0 auto;
+        padding: 0 20px;
+        padding-top: 80px;
+    }
+    &__content {
+        background: var(--white-color);
+        border-radius: 25px;
+        padding: 50px;
+        padding-bottom: 41px;
+    }
+}
+.list-reset {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+@font-face {
+    font-family: Montserrat;
+    src: url(./assets/fonts/Montserrat-Regular.woff2) format('woff2');
+    font-weight: 400;
+    font-display: swap;
+    font-style: normal;
+}
+@font-face {
+    font-family: Montserrat;
+    src: url(./assets/fonts/Montserrat-Medium.woff2) format('woff2');
+    font-weight: 500;
+    font-display: swap;
+    font-style: normal;
+}
+@font-face {
+    font-family: Montserrat;
+    src: url(./assets/fonts/Montserrat-SemiBold.woff2) format('woff2');
+    font-weight: 600;
+    font-display: swap;
+    font-style: normal;
+}
+@font-face {
+    font-family: Montserrat;
+    src: url(./assets/fonts/Montserrat-Bold.woff2) format('woff2');
+    font-weight: 700;
+    font-display: swap;
+    font-style: bold;
+}
+@font-face {
+    font-family: 'Open Sans';
+    src: url(./assets/fonts/OpenSans-Regular.woff2) format('woff2');
+    font-weight: 400;
+    font-display: swap;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'Open Sans';
+    src: url(./assets/fonts/OpenSans-SemiBold.woff2) format('woff2');
+    font-weight: 500;
+    font-display: swap;
+    font-style: normal;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,118 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {
+        citiesList: [
+            'Алматы',
+            'Челябинск',
+            'Екатеринбург',
+            'Нижний Новгород',
+            'Казань',
+            'Краснодар',
+            'Красноярск',
+            'Новосибирск',
+            'Пермь',
+            'Самара',
+            'Тюмень',
+            'Уфа',
+        ],
+        currentCity: 'Екатеринбург',
+        currentFillialToggler: 'ДокторОст',
+        currentPeriod: '7',
+        doctorsList: [
+            {
+                title: 'Кузина Оксана Владимировна',
+                job: 'Невролог, рефлексотерапевт, цефалголог, гирудотерапевт',
+                isOnline: false,
+                isDoctor: true,
+            },
+            {
+                title: 'Максимова Татьяна Петровна',
+                job: 'Главный врач, невролог высшей категории',
+                isOnline: true,
+                isDoctor: true,
+            },
+            {
+                title: 'Логвинова Светлана Васильевна',
+                job: 'Невролог первой категории, рефлексотерапевт',
+                isOnline: true,
+                isDoctor: true,
+            },
+            {
+                title: 'Самонова Наталья Анатольевна',
+                job: 'Невролог первой категории, рефлексотерапевт',
+                isOnline: true,
+                isDoctor: true,
+            },
+            {
+                title: 'Самонова Наталья Анатольевна',
+                job: 'Невролог первой категории, рефлексотерапевт',
+                isOnline: true,
+                isDoctor: true,
+            },
+            {
+                title: 'Самонова Наталья Анатольевна',
+                job: 'Невролог первой категории, рефлексотерапевт',
+                isOnline: true,
+                isDoctor: true,
+            },
+            {
+                image: require('../assets/img/cabinet-icon-1.svg'),
+                title: '303 кабинет',
+                job: 'Процедурный',
+                isDoctor: false,
+            },
+            {
+                image: require('../assets/img/cabinet-icon-2.svg'),
+                title: '305 кабинет',
+                job: 'Массажный',
+                isDoctor: false,
+            },
+            {
+                image: require('../assets/img/cabinet-icon-2.svg'),
+                title: '305 кабинет',
+                job: 'Массажный',
+                isDoctor: false,
+            },
+            {
+                image: require('../assets/img/cabinet-icon-2.svg'),
+                title: '305 кабинет',
+                job: 'Массажный',
+                isDoctor: false,
+            },
+            {
+                image: require('../assets/img/cabinet-icon-2.svg'),
+                title: '305 кабинет',
+                job: 'Массажный',
+                isDoctor: false,
+            },
+            {
+                image: require('../assets/img/cabinet-icon-2.svg'),
+                title: '305 кабинет',
+                job: 'Массажный',
+                isDoctor: false,
+            },
+            {
+                image: require('../assets/img/cabinet-icon-2.svg'),
+                title: '305 кабинет',
+                job: 'Массажный',
+                isDoctor: false,
+            },
+        ],
+        activeItemIndex: null,
+    },
+    getters: {},
+    mutations: {
+        updateCurrentFillial(state, newFillial) {
+            state.currentFillialToggler = newFillial;
+        },
+        updateCurrentPeriod(state, newPeriod) {
+            state.currentPeriod = newPeriod;
+        },
+        changeActiveItemIndex(state, idx) {
+            state.activeItemIndex = idx;
+        },
+    },
+    actions: {},
+    modules: {},
+});
